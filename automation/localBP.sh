63cf384cff67f34bed128c3661c5ae9c05cb7a50
Vignesh <vigneshkmr84@gmail.com>
2021-11-06 17:17:24 -0500

    LocalBP.sh script updated; removed bug in setting up unused variables

--- automation/localBP.sh
+++ automation/localBP.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-
-#if [[ "$#" != 3 ]]; then 
-#    echo "Please provide 3 arguments. Exitting..."
-#    exit -1
-#fi
-
-#echo "Input arguments"
-#echo "$@"
-
-#PRED_FILE_PATH=~/gem5/gem5/src/cpu/pred
-#CPU_FILE_PATH=~/gem5/gem5/src/cpu/simple
-PRED_FILE_PATH=.
-CPU_FILE_PATH=.
-#RESULTS_DIR=~/results_new
-RESULTS_DIR=./results_new
+PRED_FILE_PATH=~/gem5/gem5/src/cpu/pred
+CPU_FILE_PATH=~/gem5/gem5/src/cpu/simple
+RESULTS_DIR=~/results_new
 GEM5_HOME_DIR=~/gem5/gem5
 BENCHMARK_HOME_DIR=~/Project1_SPEC
-
+HOME_DIR=~
 
 
 updatePredictor(){
@@ -30,9 +18,18 @@ updatePredictor(){
 
     echo "Updating the BTB & Local Predictor values"
 
+    # Updating unused values to avoid build Error
+    sed -i'.bakup' "s/T_LOCAL_VAR/${2}/" "$PRED_FILE_PATH/BranchPredictor.py"
+
     ## In place update for both BSD & GNU based unix
-    sed -i'.bakup' "s/\|BTB\|/${1}/g" "$PRED_FILE_PATH/BranchPredictor.py" 
-    sed -i'.bakup' "s/\|LocalBP\|/${2}/" "$PRED_FILE_PATH/BranchPredictor.py"
+    sed -i'.bakup' "s/BTB_VAR/${1}/g" "$PRED_FILE_PATH/BranchPredictor.py"
+    sed -i'.bakup' "s/LOCAL_VAR/${2}/" "$PRED_FILE_PATH/BranchPredictor.py"
+
+    # Updating unused values to avoid build Error
+    sed -i'.bakup' "s/B_GLOBAL_VAR/${2}/" "$PRED_FILE_PATH/BranchPredictor.py"
+    sed -i'.bakup' "s/B_CHOICE_VAR/${2}/" "$PRED_FILE_PATH/BranchPredictor.py"
+    sed -i'.bakup' "s/T_GLOBAL_VAR/${2}/" "$PRED_FILE_PATH/BranchPredictor.py"
+    sed -i'.bakup' "s/T_CHOICE_VAR/${2}/" "$PRED_FILE_PATH/BranchPredictor.py"
 
     echo "Completed updating values"
     cat $PRED_FILE_PATH/BranchPredictor.py
@@ -47,8 +44,9 @@ updateCPU(){
 
 build(){
     echo "Deleting X86 build dir"
-    #rm -rf $GEM5_HOME_DIR/build/X86/
-    #scons build/X86/gem5.opt -j5
+    rm -rf $GEM5_HOME_DIR/build/X86/
+    cd $GEM5_HOME_DIR
+    scons build/X86/gem5.opt -j5
 }
 
 runSimulation(){
@@ -63,31 +61,30 @@ runSimulation(){
     bash newRun.sh
 }
 
-# default results will be generated at ~/m5out_470 & ~/m5out_458 directory 
+# default results will be generated at ~/m5out_470 & ~/m5out_458 directory
 backupResults(){
-    echo "Backing up dir - $1 to $1_btb_$2_local_$3"
-    mv "$1" "$RESULTS_DIR/$1_btb_$2_local_$3"
+    echo "Backing up dir - $HOME_DIR/$1 to $RESULTS_DIR/$1/btb_$2/local_$3"
+    mv -vf "$HOME_DIR/$1" "$RESULTS_DIR/$1/btb_$2/local_$3"
 }
 
 
-main() { 
+main() {
     echo "Predictor - $1; BTB - $2; Local Predictor - $3"
     updatePredictor $2 $3
     build
     runSimulation $2 $3
-    backupResults m5out_458 $2 $3 
-    backupResults m5out_470 $2 $3 
+    cd ~
+    backupResults m5out_458 $2 $3
+    backupResults m5out_470 $2 $3
 }
 
-updateCPU $1
+updateCPU "LocalBP()"
 echo "\n"
+
 main "LocalBP()" 4096 2048
-#main "LocalBP()" 4096 1024
-#main "LocalBP()" 2048 2048
-#main "LocalBP()" 2048 1024
+main "LocalBP()" 4096 1024
 
+main "LocalBP()" 2048 2048
+main "LocalBP()" 2048 1024
 
-
-
-### ERROR BACKUP 
-###/home/012/v/vx/vxt200003/results_new/m5out_470/btb_4096/local_2048/m5out_470
+#main "LocalBP()" 2048 512
